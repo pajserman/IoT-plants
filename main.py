a93b4d2663b67920812bbcf63eb9519b9e2188fa
Hannes_Manssson <hannes.mansson01@gmail.com>
2024-06-08 18:02:46 +0200

    almost done

--- main.py
+++ main.py
@@ -1,18 +1,74 @@
+import machine
 from machine import Pin
 import time
 from dht import DHT11
+import keys
+import network
+from umqttsimple import MQTTClient
+import ubinascii
 
-
+# Pin variables
 waetherPin = Pin(27, Pin.OUT, Pin.PULL_DOWN)
 weatherSensor = DHT11(waetherPin)
-
 lightPin = Pin(15, Pin.IN, Pin.PULL_DOWN)
+led = Pin("LED", machine.Pin.OUT)
 
-while True:
+#WIFI variables
+ssid, password = keys.WIFI_SSID, keys.WIFI_PASS
+
+# MQTT variables
+MQTT_BROKER = "192.168.1.226"
+MQTT_TOPIC_TEMP = "Pico/sensor/Temperature"
+MQTT_TOPIC_HUMIDITY = "Pico/sensor/Humidity"
+MQTT_TOPIC_SUNSHINE = "Pico/sensor/Sunshine"
+
+#setup MQTTClient
+client_id = ubinascii.hexlify(machine.unique_id())
+mqtt_client = MQTTClient(client_id, MQTT_BROKER)
+mqtt_client.connect()
+
+def gather_data():
     weatherSensor.measure()
     tempC = weatherSensor.temperature()
-    humidity = weatherSensor.humidity()
-    sunshine = 1 if lightPin.value() == 0 else 0
-    data = [tempC, humidity, sunshine]
-    print(data)
-    time.sleep(20)
+    hum = weatherSensor.humidity()
+    sun = (lightPin.value() + 1) % 2
+    data = {'temperature': str(tempC), 'humidity': str(hum), 'sunshine': str(sun)}
+    return data 
+
+# wlan needs to be global, defined outside of function
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+def connect_to_wifi():
+    led.on()
+    limit = 0
+    wlan.connect(ssid, password)
+    while(wlan.isconnected() == False):
+        print("Waiting for connection...")
+        limit += 1
+        if(limit > 10):
+            limit = 0
+            print("Time limit reached. Trying again...")
+            wlan.connect(ssid, password)
+        time.sleep(1)
+    led.off()
+    print("Connected to WIFI", "IP Adress is: " +  wlan.ifconfig()[0])
+
+
+# Connecting to WIFI
+connect_to_wifi()
+
+# THE MAIN LOOP
+while(True):
+    while(wlan.isconnected() == True):
+        data = gather_data() #data {temp, hum, sun}
+        print(data)
+
+        # publish data
+        led.toggle()
+        mqtt_client.publish(MQTT_TOPIC_TEMP, data['temperature'])
+        mqtt_client.publish(MQTT_TOPIC_HUMIDITY, data['humidity'])
+        mqtt_client.publish(MQTT_TOPIC_SUNSHINE, data['sunshine'])
+        led.toggle()
+        time.sleep(10)
+    else:
+        connect_to_wifi()
